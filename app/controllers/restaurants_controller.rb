681cdc975ef05a91a67db930b8164e0fb46833cf
waninokoproject <shironekoproject.1109@gmail.com>
2020-03-14 17:00:47 +0000

    refacror layout
    cleansed index.slim layout

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,30 +4,32 @@ class RestaurantsController < ApplicationController
     require 'net/https'
     require "uri"
 
-    word="ラーメン"
+    @hentai = "tintin"
 
     data={
       "keyid": "272a4e4561b20e404f0c4f59e7fd22a2",
-      #"freeword": word,
-      "hit_per_page": 10,
-      "offset": 1,
-      "id": "ge2t300"
+      #{}"freeword": word,
+      "input_coordinates_mode": 1,
+      "latitude": 34.69,
+      "longitude": 135.1955,
+      "hit_per_page": 10
     }
 
     query=data.to_query #ハッシュをURL文字列に変換
     uri = URI("https://api.gnavi.co.jp/RestSearchAPI/20150630/?"+query)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    req = Net::HTTP::Get.new(uri)
-    @res = http.request(req)
-    #res_data = Hash.from_json(res.body)
-
+    #http = Net::HTTP.new(uri.host, uri.port)
+    #http.use_ssl = true
+    #req = Net::HTTP::Get.new(uri)
+    #res = http.request(req)
 
     begin
       response = Net::HTTP.new(uri.host, uri.port).yield_self do |http|
         http.use_ssl = true
+        # 接続時に待つ最大秒数を設定
         http.open_timeout = 5
+        # 読み込み一回でブロックして良い最大秒数を設定
         http.read_timeout = 30
+        # Net::HTTPResponseのインスタンスが返ってくる
         http.get(uri.request_uri)
       end
 
@@ -42,8 +44,10 @@ class RestaurantsController < ApplicationController
         # puts @hash ["rest"][2]["name"]
       when Net::HTTPRedirection
         logger.warn("Redirection: code=#{response.code} message=#{response.message}")
+        @message = "Redirection: code=#{response.code} message=#{response.message}"
       else
         logger.error("HTTP ERROR: code=#{response.code} message=#{response.message}")
+        @message = "HTTP ERROR: code=#{response.code} message=#{response.message}"
       end
     rescue IOError => e
       logger.error(e.message)
